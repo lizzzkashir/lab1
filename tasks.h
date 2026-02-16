59a435f5b205cd8847cfe6d341e5fe87e99561f4
lizzzkashir <m5b45dwrqj@privaterelay.appleid.com>
2026-02-16 19:55:46 +0500

    Create tasks.h

---
+++ tasks.h
@@ -0,0 +1,20 @@
+#ifndef TASKS_H
+#define TASKS_H
+
+//BackRec10
+
+void r10(const char* filename);
+void REc10();
+void rec10m();
+
+//HomeDyn1
+void din1fil(const char* filename);
+void dyn1cons();
+void dyn1m();
+
+//HomeDyn17
+void dyn17f(const char* filename);
+void dyndyn17con();
+void dyndyn17m();
+
+#endif
